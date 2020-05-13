50e48bbe27b2bbbab0b3e827239bbf72a3a75571
Aamir Ansari <aamir.ansari1104@gmail.com>
2020-05-13 16:35:21 +0530

    cms Template

---
+++ cmstemplate/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+
+
+/*
+
+|--------------------------------------------------------------------------
+
+| Application Routes
+
+|--------------------------------------------------------------------------
+
+|
+
+| Here is where you can register all of the routes for an application.
+
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+
+| and give it the Closure to execute when that URI is requested.
+
+|
+
+*/
+
+
+/**
+ * Group Route to redirect all the routes with prefix faculty
+ */
+
+
+Route::group(['prefix'=>'staff', 'middleware' => 'admin'], function() {
+
+    Route::get('/home', 'FacultyController@index');
+    
+    Route::get('/attendance/faculty', 'FacultyController@facultyattendance');
+    Route::get('search/', 'FacultyController@searchStudent');
+
+});
+
+
+
+Route::group(['prefix' => 'theme'], function(){
+    Route::get('/red', 'ThemeController@red');
+    Route::get('/blue', 'ThemeController@blue');
+});
+
+Route::get('login', 'OauthController@login');
+//Route::get('social/redirect/google', 'OauthController@redirectToProvider');
+//Route::get('social/handle/google', 'OauthController@handleProviderCallback');
+Route::resource('remarks', 'CommentController');
+Route::get('/logout', 'SessionController@logout');
+Route::get('/', 'SessionController@home');
+
+?>
+
+
